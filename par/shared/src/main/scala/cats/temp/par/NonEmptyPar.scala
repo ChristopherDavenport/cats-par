3c865046234a27fa8b8dbe202a72c413ded17a57
Scala Steward <me@scala-steward.org>
2020-02-25 14:14:33 +0100

    Update cats-core to 2.1.1

--- par/shared/src/main/scala/cats/temp/par/NonEmptyPar.scala
+++ par/shared/src/main/scala/cats/temp/par/NonEmptyPar.scala
@@ -2,20 +2,20 @@ package cats.temp.par
 
 import cats.NonEmptyParallel
 
-@deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.0.0", since = "1.0.0-RC2")
+@deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.1.1", since = "1.0.0-RC2")
 trait NonEmptyPar[F[_]]{
   type NonEmptyParAux[A]
   def nonEmptyParallel: NonEmptyParallel.Aux[F, NonEmptyParAux]
 }
 
 object NonEmptyPar {
-  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.0.0", since = "1.0.0-RC2")
+  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.1.1", since = "1.0.0-RC2")
   def apply[F[_]](implicit ev: NonEmptyPar[F]) = ev
 
-  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.0.0", since = "1.0.0-RC2")
+  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.1.1", since = "1.0.0-RC2")
   type Aux[F[_], G[_]] = NonEmptyPar[F]{type NonEmptyParAux[A] = G[A]}
 
-  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.0.0", since = "1.0.0-RC2")
+  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.1.1", since = "1.0.0-RC2")
   implicit def fromNonEmptyParallel[F[_], G[_]](implicit P: NonEmptyParallel.Aux[F, G]): NonEmptyPar.Aux[F, G] =
     new NonEmptyPar[F]{
       type NonEmptyParAux[A] = G[A]
--- par/shared/src/main/scala/cats/temp/par/package.scala
+++ par/shared/src/main/scala/cats/temp/par/package.scala
@@ -3,12 +3,12 @@ package cats.temp
 import cats.{NonEmptyParallel, Parallel}
 
 package object par extends NonEmptyParConversion {
-  @deprecated("Use Cats Parallel Directly - Fixed by cats 2.0.0", since = "1.0.0-RC2")
+  @deprecated("Use Cats Parallel Directly - Fixed by cats 2.1.1", since = "1.0.0-RC2")
   implicit def parToParallel[F[_]](implicit P: Par[F]): Parallel.Aux[F, P.ParAux] = P.parallel
 }
 
 private[temp] sealed abstract class NonEmptyParConversion {
-  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.0.0", since = "1.0.0-RC2")
+  @deprecated("Use Cats NonEmptyParallel Directly - Fixed by cats 2.1.1", since = "1.0.0-RC2")
   implicit def nonEmptyParToNonEmptyParallel[F[_]](implicit P: par.NonEmptyPar[F]): NonEmptyParallel.Aux[F, P.NonEmptyParAux] =
     P.nonEmptyParallel
 }
